9100ee7e77c9cd7d74fbcbfe29e31ae7211374b7
ace00029 <alvaro.castro@tprs.stud.vu.lt>
2022-09-22 19:21:25 +0300

    Added change text button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
+    public void changeText(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.mainText);
+        tv.setText("Welcome to android");
+    }
 }
